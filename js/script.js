dbb4b07680ed7b3f5e9f0ba7f229e85af4a10784
yurika0501 <titizuzu0501@gmail.com>
2023-05-22 07:44:18 +0000

    [Add]画像表示の繰り返し処理

--- js/script.js
+++ js/script.js
@@ -1,4 +1,17 @@
 const menu = document.querySelector('#menu');
 
-const content = `<div><img src="images/strawberry.jpg" alt""></div>`;
-menu.insertAdjacentHTML('beforeend', content);
+const lists = [
+    'strawberry.jpg',
+    'lime.jpg',
+    'mango.jpg',
+    'lemon.jpg',
+    'fig.jpg',
+    'apple.jpg',
+];
+
+console.log(lists.length);
+
+for(let i = 0; i < lists.length; i++){
+    const content = `<div><img src="images/${lists[i]}" alt=""></div>`;
+    menu.insertAdjacentHTML('beforeend', content);
+}
